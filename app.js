62100a7feeec228fb2fc444d2b9ad5fc1faade28
styp3r <stypertypes@gmail.com>
2021-07-19 04:06:26 +0530

    add animation to button click

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+$(".g1, .g2, .g3").toggle();
 $(".btn").click(()=>{
   console.log("Button Clicked!");
-  $("#loginUI").removeClass("purple").addClass("blue");
+  $("#loginUI").animate({height: "250px"}, 200);
+  $("#loginUI").toggleClass("collapse");
+
+
+  if ($(".btn").text() == "v"){
+       $(".btn").text("^");
+     }
+    else{
+       $(".btn").text("v");
+       $("#loginUI").animate({height: "100px"}, 200);
+     }
+
+       $(".g1, .g2, .g3").fadeToggle(250);
 });
