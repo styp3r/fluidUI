9b1519d4ce2a571863466a906124985a39aa431b
styp3r <stypertypes@gmail.com>
2021-07-21 03:40:04 +0530

    add functionality to buttons with animations

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-$(".g1, .g2, .g3").toggle();
-$(".btn").click(()=>{
+$(".signUpForm").addClass("invisible");
+$(".signUp").click(()=>{
   console.log("Button Clicked!");
-  $("#loginUI").animate({height: "250px"}, 200);
-  $("#loginUI").toggleClass("collapse");
+  $(".signUpForm").removeClass("invisible");
+  $("#loginUI").animate({height: "450px"}, 200);
+  $("#signupUI").animate({height: "300px"}, 200);
 
-
-  if ($(".btn").text() == "v"){
-       $(".btn").text("^");
+  if ($(".signUp").html()=="Or Sign Up here"){
+       $(".signUp").html("Have an account already?").append("<a href = '#'> Log In</a></p>");
      }
     else{
-       $(".btn").text("v");
-       $("#loginUI").animate({height: "100px"}, 200);
+       $(".signUp").html("Or Sign Up here");
+       $(".signUpForm").addClass("invisible");
+       $("#loginUI").animate({height: "400px"}, 100);
+       $("#signupUI").animate({height: "0"}, 150);
      }
-
-       $(".g1, .g2, .g3").fadeToggle(250);
 });
